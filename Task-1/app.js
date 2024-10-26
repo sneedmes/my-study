836ba899062fe5c3ab9fbc668606eaa6bbd34221
sneedmes <dallaqyan0610@gmail.com>
2024-10-26 21:54:32 +0400

    Выполнено первое задание: повторяющиеся элементы

---
+++ Task-1/app.js
@@ -0,0 +1,29 @@
+let array = []
+const countElements = (array) => {
+    const count = {};
+    array.forEach(f => {
+        if (!count[f]) {
+            count[f] = 1;
+        } else {
+            count[f] ++;
+        }
+    });
+    return count;
+};
+btn.onclick = function (){
+    let elem1 = document.getElementById("elem-1").value
+    let elem2 = document.getElementById("elem-2").value
+    let elem3 = document.getElementById("elem-3").value
+    let elem4 = document.getElementById("elem-4").value
+    let elem5 = document.getElementById("elem-5").value
+    let elem6 = document.getElementById("elem-6").value
+    array.push(elem1, elem2, elem3, elem4, elem5, elem6)
+    let result = JSON.stringify(countElements(array))
+    document.getElementById("result").innerHTML="<h3>Результат выполнения: " + result + "</h3>"
+    array = []
+}
+
+
+
+
+
