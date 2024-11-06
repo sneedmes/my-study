1fc5c466a9e9559660ce9ca350ed32394216bfa3
sneedmes <dallaqyan0610@gmail.com>
2024-11-06 23:59:44 +0400

    feat: crate new web-site AdminDashboard

---
+++ AdminDashboard/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '16367',
+        paymenStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'React lessons',
+        productNumber: '11111',
+        paymenStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'TypeScript course',
+        productNumber: '78727',
+        paymenStatus: 'Paid',
+        status: 'Acting'
+    }
+]
