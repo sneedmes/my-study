d084f3c0e736f3f449f6d54c9ba4f344d41fc06f
sneedmes <dallaqyan0610@gmail.com>
2024-12-27 22:07:04 +0400

    feat: added new buttons and actions for multiplying and divorcing

--- JScourse/app.js
+++ JScourse/app.js
@@ -1,13 +1,56 @@
 const resultElement = document.getElementById('result')
 const submitBtn = document.getElementById('submit')
+const input1 = document.getElementById('input1')
+const input2 = document.getElementById('input2')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+const multiplyBtn = document.getElementById('multiply')
+const divorceBtn = document.getElementById('divorce')
+let action = "+"
 
+plusBtn.onclick = function (){
+    action = "+"
+}
 
+minusBtn.onclick = function (){
+    action = "-"
+}
+
+multiplyBtn.onclick = function (){
+    action = "*"
+}
+
+divorceBtn.onclick = function (){
+    action = "/"
+}
+
+function printResult(result) {
+    if (result < 0) {
+        resultElement.style.color = "red"
+    } else {
+        resultElement.style.color = "green"
+    }
+    resultElement.textContent = result
+}
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+    const num1 = Number(inp1.value)
+    const num2 = Number(inp2.value)
+    if (actionSymbol === "+") {
+        return num1 + num2
+    } else if (actionSymbol === "-") {
+        return num1 - num2
+    } else if (actionSymbol === "*"){
+        return num1 * num2
+    } else if (actionSymbol === "/") {
+        return num1 / num2
+    }
+}
 
 submitBtn.onclick = function (){
-    resultElement.textContent = sum
-    const input1 = document.getElementById('input1')
-    const input2 = document.getElementById('input2')
-    const sum = Number(input1.value) + Number(input2.value)
-
+    const result = computeNumbersWithAction(input1, input2, action)
+    printResult(result)
 }
+
+
 
